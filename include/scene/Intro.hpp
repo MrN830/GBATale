6698d58a268a39be1c77295ea5a84523fc7289dc
copyrat90 <copyrat90@gmail.com>
2023-05-20 21:33:17 +0900

    Add `Intro` scene fade & move actions

--- include/scene/Intro.hpp
+++ include/scene/Intro.hpp
@@ -2,6 +2,12 @@
 
 #include "scene/Scene.hpp"
 
+#include <bn_blending_actions.h>
+#include <bn_fixed_rect.h>
+#include <bn_optional.h>
+#include <bn_regular_bg_actions.h>
+#include <bn_regular_bg_ptr.h>
+
 namespace ut::scene
 {
 
@@ -9,9 +15,27 @@ class Intro final : public Scene
 {
 public:
     Intro(SceneStack&, Context&);
+    ~Intro();
 
     bool handleInput() override;
     bool update() override;
+
+private:
+    void updateBgFade();
+    void startNextBgFade();
+
+    void updateBgMove();
+
+private:
+    const bn::fixed_rect _prevWindowRect;
+
+    bn::regular_bg_ptr _bg;
+    bn::blending_transparency_alpha_to_action _bgFade;
+    bn::optional<bn::regular_bg_move_to_action> _bgMove;
+
+    int _elapsedFrames = 0;
+    int _introBgIdx0 = 0;
+    bool _isFadeOut = false;
 };
 
 } // namespace ut::scene
--- src/scene/Intro.cpp
+++ src/scene/Intro.cpp
@@ -1,10 +1,101 @@
 #include "scene/Intro.hpp"
 
+#include <bn_display.h>
+#include <bn_rect_window.h>
+#include <bn_regular_bg_builder.h>
+
+#include "bn_regular_bg_items_bg_intro1.h"
+#include "bn_regular_bg_items_bg_intro10.h"
+#include "bn_regular_bg_items_bg_intro2.h"
+#include "bn_regular_bg_items_bg_intro3.h"
+#include "bn_regular_bg_items_bg_intro4.h"
+#include "bn_regular_bg_items_bg_intro5.h"
+#include "bn_regular_bg_items_bg_intro6.h"
+#include "bn_regular_bg_items_bg_intro7.h"
+#include "bn_regular_bg_items_bg_intro8.h"
+#include "bn_regular_bg_items_bg_intro9.h"
+
 namespace ut::scene
 {
 
-Intro::Intro(SceneStack& sceneStack, Context& context) : Scene(sceneStack, context)
+namespace
 {
+
+struct IntroBgFade
+{
+    const bn::regular_bg_item* bgItem;
+    int fadeInStart;
+    int fadeOutStart;
+};
+
+constexpr int FPS = 30;
+constexpr int INTRO_BG_COUNT = 10;
+
+constexpr int FADE_IN_FRAMES = 10;
+constexpr int FADE_OUT_FRAMES = 9;
+constexpr int LAST_FADE_IN_FRAMES = 7;
+constexpr int LAST_FADE_OUT_FRAMES = 20;
+
+constexpr int BG_MOVE_START_FRAME = 61 * FPS + 19;
+constexpr int BG_MOVE_FRAMES = 229;
+
+constexpr IntroBgFade INTRO_BGS[INTRO_BG_COUNT] = {
+    {&bn::regular_bg_items::bg_intro1, -1, 6 * FPS + 15},
+    {&bn::regular_bg_items::bg_intro2, 6 * FPS + 29, 12 * FPS + 3},
+    {&bn::regular_bg_items::bg_intro3, 12 * FPS + 17, 17 * FPS + 27},
+    {&bn::regular_bg_items::bg_intro4, 18 * FPS + 11, 24 * FPS + 0},
+    {&bn::regular_bg_items::bg_intro5, 29 * FPS + 5, 34 * FPS + 0},
+    {&bn::regular_bg_items::bg_intro6, 34 * FPS + 14, 40 * FPS + 15},
+    {&bn::regular_bg_items::bg_intro7, 40 * FPS + 29, 45 * FPS + 18},
+    {&bn::regular_bg_items::bg_intro8, 46 * FPS + 2, 51 * FPS + 3},
+    {&bn::regular_bg_items::bg_intro9, 51 * FPS + 17, 56 * FPS + 21},
+    {&bn::regular_bg_items::bg_intro10, 57 * FPS + 8, 74 * FPS + 27},
+};
+
+constexpr int NEXT_SCENE_FRAMES = 75 * FPS + 27;
+
+constexpr bn::fixed_point LAST_BG_START_POS = {8, -122};
+constexpr bn::fixed_point LAST_BG_END_POS = {8, 91};
+
+auto buildIntroBg(int idx0) -> bn::regular_bg_ptr
+{
+    BN_ASSERT(0 <= idx0 && idx0 < INTRO_BG_COUNT);
+
+    bn::regular_bg_builder builder(*INTRO_BGS[idx0].bgItem);
+    builder.set_blending_enabled(true);
+    if (idx0 == INTRO_BG_COUNT - 1)
+        builder.set_position(LAST_BG_START_POS);
+
+    auto bg = builder.build();
+    bn::rect_window::outside().set_show_bg(bg, false);
+
+    return bg;
+}
+
+auto buildFadeIn(int frames) -> bn::blending_transparency_alpha_to_action
+{
+    return bn::blending_transparency_alpha_to_action(frames, 1);
+}
+
+auto buildFadeOut(int frames) -> bn::blending_transparency_alpha_to_action
+{
+    return bn::blending_transparency_alpha_to_action(frames, 0);
+}
+
+} // namespace
+
+Intro::Intro(SceneStack& sceneStack, Context& context)
+    : Scene(sceneStack, context), _prevWindowRect(bn::rect_window::internal().boundaries()), _bg(buildIntroBg(0)),
+      _bgFade(1, 1)
+{
+    bn::rect_window::internal().set_boundaries(6 - bn::display::height() / 2, 32 - bn::display::width() / 2,
+                                               102 - bn::display::height() / 2, 208 - bn::display::width() / 2);
+}
+
+Intro::~Intro()
+{
+    bn::blending::set_transparency_alpha(1);
+    bn::rect_window::internal().set_boundaries(_prevWindowRect);
 }
 
 bool Intro::handleInput()
@@ -14,7 +105,60 @@ bool Intro::handleInput()
 
 bool Intro::update()
 {
+    ++_elapsedFrames;
+
+    updateBgFade();
+    updateBgMove();
+
     return true;
+}
+
+void Intro::updateBgFade()
+{
+    if (!_bgFade.done())
+    {
+        // fade-in is updated on every 2 frames
+        if (_isFadeOut || _elapsedFrames % 2 == 0)
+            _bgFade.update();
+
+        if (_bgFade.done() && _isFadeOut)
+        {
+            if (++_introBgIdx0 < INTRO_BG_COUNT)
+                _bg = buildIntroBg(_introBgIdx0);
+        }
+    }
+
+    if (_introBgIdx0 < INTRO_BG_COUNT)
+        startNextBgFade();
+}
+
+void Intro::startNextBgFade()
+{
+    BN_ASSERT(0 <= _introBgIdx0 && _introBgIdx0 < INTRO_BG_COUNT);
+
+    const auto& curBgFade = INTRO_BGS[_introBgIdx0];
+
+    if (_elapsedFrames == curBgFade.fadeInStart)
+    {
+        _isFadeOut = false;
+        const int fadeInFrames = (_introBgIdx0 == INTRO_BG_COUNT - 1) ? LAST_FADE_IN_FRAMES : FADE_IN_FRAMES;
+        _bgFade = buildFadeIn(fadeInFrames);
+    }
+    else if (_elapsedFrames == curBgFade.fadeOutStart)
+    {
+        _isFadeOut = true;
+        const int fadeOutFrames = (_introBgIdx0 == INTRO_BG_COUNT - 1) ? LAST_FADE_OUT_FRAMES : FADE_OUT_FRAMES;
+        _bgFade = buildFadeOut(fadeOutFrames);
+    }
+}
+
+void Intro::updateBgMove()
+{
+    if (_elapsedFrames == BG_MOVE_START_FRAME)
+        _bgMove = bn::regular_bg_move_to_action(_bg, BG_MOVE_FRAMES, LAST_BG_END_POS);
+
+    if (_bgMove && !_bgMove->done())
+        _bgMove->update();
 }
 
 } // namespace ut::scene
