9d568cfbd0062a4a9fa9d50b2ba1a930239ef03c
copyrat90 <copyrat90@gmail.com>
2023-06-07 02:05:20 +0900

    Add mtileset generation

---
+++ tools/tilemap_converter.py
@@ -0,0 +1,68 @@
+import os
+from pytmx import pytmx
+from PIL import Image
+from collections import namedtuple
+
+
+class TilemapConverter:
+    class TooManyMTilesException(Exception):
+        def __init__(self, mtile_count):
+            super().__init__(f"Too many MTiles: {mtile_count} (max 255)")
+            self.mtile_count = mtile_count
+
+    @staticmethod
+    def convert(tmx_path: str):
+        """Convert a single `*.tmx` tilemap"""
+        tiled_map = pytmx.TiledMap(tmx_path)
+        bg_upper: pytmx.TiledTileLayer = tiled_map.get_layer_by_name("BGUpper")
+        bg_upper2: pytmx.TiledTileLayer = tiled_map.get_layer_by_name("BGUpper2")
+        bg_lower: pytmx.TiledTileLayer = tiled_map.get_layer_by_name("BGLower")
+        objects: pytmx.TiledObjectGroup = tiled_map.get_layer_by_name("Objects")
+
+        # Test
+        TilemapConverter.__generate_mtileset(
+            bg_upper2, os.path.splitext(os.path.basename(tmx_path))[0], tiled_map
+        )
+
+    @staticmethod
+    def __generate_mtileset(
+        tile_layer: pytmx.TiledTileLayer, tilemap_name: str, tiled_map: pytmx.TiledMap
+    ):
+        tileset_rects = {}
+        for x, y, gid in tile_layer.iter_data():
+            if gid != 0:
+                img_path, img_rect, tile_flags = tiled_map.get_tile_image_by_gid(gid)
+                if img_path not in tileset_rects:
+                    tileset_rects[img_path] = set()
+                tileset_rects[img_path].add((gid, img_rect))
+
+        mtile_count = sum(len(v) for v in tileset_rects.values())
+        if mtile_count >= 255:
+            raise TilemapConverter.TooManyMTilesException(mtile_count)
+
+        mtileset = Image.new("RGBA", (16, 16 * (mtile_count + 1)))
+        gid_mtile_idx_mapping = {}
+
+        for tileset_path, gid_rects in tileset_rects.items():
+            with Image.open(tileset_path) as tileset:
+                for gid, rect in gid_rects:
+                    mtile_idx = len(gid_mtile_idx_mapping) + 1
+                    gid_mtile_idx_mapping[gid] = mtile_idx
+                    x, y, w, h = rect
+                    mtile = tileset.crop((x, y, x + w, y + h))
+                    mtileset.paste(mtile, (0, h * mtile_idx))
+                    pass
+
+        mtilemap_root = f"build_ut/mtilemap/"
+        os.makedirs(mtilemap_root, exist_ok=True)
+
+        mtileset = mtileset.quantize(16)
+        mtileset.save(os.path.join(mtilemap_root, f"{tilemap_name}.bmp"))
+        mtileset.close()
+
+        return gid_mtile_idx_mapping
+
+
+if __name__ == "__main__":
+    # Test
+    TilemapConverter.convert("extra/tilemaps/Ruins.xml")
