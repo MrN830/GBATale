22e7b1250c5e658d47369fdc5d28e14f4857bc81
copyrat90 <copyrat90@gmail.com>
2023-06-06 09:22:56 +0900

    Fix image path in `Ruins.tsx`

--- extra/tilemaps/Ruins.tsx
+++ extra/tilemaps/Ruins.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.2" name="Ruins1" tilewidth="16" tileheight="16" tilecount="1360" columns="40">
- <image source="../../Tilesets/RuinsTilesSm.png" width="640" height="544"/>
+ <image source="ruins_tileset.png" width="640" height="544"/>
 </tileset>
