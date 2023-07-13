09ddb7ac15a9e099d19f2e62d99a5bd36a3fb40d
copyrat90 <copyrat90@gmail.com>
2023-07-13 18:31:55 +0900

    Move tilemap entities from `BG` -> `Entity` layer
    * Move entity sprites from `ruins_tileset.png` to each sprite
    * Add some missing entities
    * Add `Entity` type in Tiled custom type
    * Remove `*Npc` types from Tiled custom types
    * Reorder layer so that `Warp` is on top of `Entity`

---
+++ extra/tilemaps/entities.tsx
@@ -0,0 +1,116 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="entities" tilewidth="64" tileheight="64" tilecount="19" columns="0" objectalignment="center">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0" x="0" y="0" width="16" height="16">
+  <image width="16" height="32" source="../../graphics/sprite/ent_floor_switch.bmp"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="2" width="14" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="1" x="0" y="0" width="16" height="16">
+  <image width="16" height="32" source="../../graphics/sprite/ent_wall_switch.bmp"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="2" y="2" width="12" height="12"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <image width="16" height="16" source="../../graphics/sprite/ent_readable_sign.bmp"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="4" width="16" height="12"/>
+  </objectgroup>
+ </tile>
+ <tile id="3" x="0" y="0" width="16" height="16">
+  <image width="16" height="32" source="../../graphics/sprite/ent_spike.bmp"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="1" width="14" height="14"/>
+  </objectgroup>
+ </tile>
+ <tile id="5" x="0" y="0" width="16" height="16">
+  <image width="16" height="32" source="../../graphics/sprite/ent_save_point.bmp"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="8" width="16" height="8"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="5" duration="166"/>
+   <frame tileid="6" duration="167"/>
+  </animation>
+ </tile>
+ <tile id="6" x="0" y="16" width="16" height="16">
+  <image width="16" height="32" source="../../graphics/sprite/ent_save_point.bmp"/>
+ </tile>
+ <tile id="7">
+  <image width="32" height="64" source="../../graphics/sprite/ent_pillar.bmp"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3" y="48" width="26" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="8" x="0" y="0" width="64" height="32">
+  <image width="64" height="64" source="../../graphics/sprite/ent_candy_dish.bmp"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="18" width="16" height="10"/>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <image width="16" height="16" source="../../graphics/sprite/ent_rock.bmp"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="2" y="7" width="12" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <image width="32" height="16" source="../../graphics/sprite/ent_cheese_table.bmp"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="2" y="8" width="19" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="11" x="0" y="0" width="16" height="16">
+  <image width="16" height="48" source="../../graphics/sprite/ent_color_switch.bmp"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3" y="7" width="10" height="9"/>
+  </objectgroup>
+ </tile>
+ <tile id="12" x="0" y="16" width="16" height="16">
+  <image width="16" height="48" source="../../graphics/sprite/ent_color_switch.bmp"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3" y="7" width="10" height="9"/>
+  </objectgroup>
+ </tile>
+ <tile id="13" x="0" y="32" width="16" height="16">
+  <image width="16" height="48" source="../../graphics/sprite/ent_color_switch.bmp"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3" y="7" width="10" height="9"/>
+  </objectgroup>
+ </tile>
+ <tile id="14">
+  <image width="16" height="16" source="../../graphics/sprite/ent_knife.bmp"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3" y="6" width="11" height="10"/>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <image width="16" height="16" source="../../graphics/sprite/ent_ribbon.bmp"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3" y="3" width="11" height="9"/>
+  </objectgroup>
+ </tile>
+ <tile id="16">
+  <image width="32" height="64" source="../../graphics/sprite/ent_ruins_door.bmp"/>
+ </tile>
+ <tile id="17">
+  <image width="16" height="16" source="../../graphics/sprite/ent_web_small.bmp"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="15"/>
+  </objectgroup>
+ </tile>
+ <tile id="18">
+  <image width="32" height="32" source="../../graphics/sprite/ent_web_big.bmp"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="27" height="26"/>
+  </objectgroup>
+ </tile>
+ <tile id="19" x="0" y="0" width="16" height="32">
+  <image width="16" height="64" source="../../graphics/sprite/ent_vegetoid.bmp"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="23" width="14" height="9"/>
+  </objectgroup>
+ </tile>
+</tileset>
