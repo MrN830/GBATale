20f9784f83bb8bbac0e85335e1242cd8f274527a
MrN830 <nfc.0131@gmail.com>
2023-08-02 15:26:12 -0400

    home initial map imports

---
+++ extra/tilemaps/home/home.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="home" tilewidth="16" tileheight="16" tilecount="1184" columns="37">
+ <image source="home_tileset.png" width="592" height="512"/>
+</tileset>
