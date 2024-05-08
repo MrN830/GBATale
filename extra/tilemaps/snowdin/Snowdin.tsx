c623badef77d94a8d2617c18c04e7f460d0e9589
MrN830 <nfc.0131@gmail.com>
2024-05-07 20:48:50 -0400

    initial commit, NO ENTITIES

---
+++ extra/tilemaps/snowdin/Snowdin.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Snowdin" tilewidth="16" tileheight="16" tilecount="3648" columns="64">
+ <image source="snowdin_tileset.png" width="1028" height="915"/>
+</tileset>
