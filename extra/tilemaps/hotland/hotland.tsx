96d474fcc15da12847a5002cea84d285f1c6bfb8
MrN830 <nfc.0131@gmail.com>
2024-08-15 19:49:28 -0400

    hotland folder created, maps 137, 138, 140 added

---
+++ extra/tilemaps/hotland/hotland.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Hotland" tilewidth="16" tileheight="16" tilecount="288" columns="18">
+ <image source="hotland_tileset.png" width="288" height="256"/>
+</tileset>
