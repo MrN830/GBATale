0609f9d5e2ebb99bb722393bc05410b046019405
MrN830 <nfc.0131@gmail.com>
2024-12-01 13:58:19 -0500

    lab tileset, room 139

---
+++ extra/tilemaps/hotland/Lab.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Lab" tilewidth="16" tileheight="16" tilecount="544" columns="34">
+ <image source="lab_tileset.png" width="544" height="256"/>
+</tileset>
