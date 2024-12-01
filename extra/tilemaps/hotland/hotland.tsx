423d3020bd41b4d789aa7883439f8a22795a9010
MrN830 <nfc.0131@gmail.com>
2024-12-01 13:55:38 -0500

    room 147

--- extra/tilemaps/hotland/hotland.tsx
+++ extra/tilemaps/hotland/hotland.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="Hotland" tilewidth="16" tileheight="16" tilecount="288" columns="18">
- <image source="hotland_tileset.png" width="288" height="256"/>
+<tileset version="1.10" tiledversion="1.10.1" name="Hotland" tilewidth="16" tileheight="16" tilecount="300" columns="20">
+ <image source="hotland_tileset.png" width="320" height="240"/>
 </tileset>
